8ff72c2458ec4c5dd823f5bd1e8747de35fa943a
pandyah5 <pandyahetav1@gmail.com>
2023-10-21 18:40:54 -0400

    Implemented comments and updated requirements.txt

--- src/nepiada/utils/noise.py
+++ src/nepiada/utils/noise.py
@@ -1,7 +1,7 @@
 from abc import ABC, abstractmethod
 import numpy as np
 
-class adversarial_noise_strategy(ABC):
+class AdversarialNoiseStrategy(ABC):
     """
     The Strategy interface declares operations common to all supported versions
     of the supported algorithms.
@@ -25,7 +25,7 @@ class AdversarialNoiseContext():
     The Context defines the interface of adversarial noise to clients.
     """
 
-    def __init__(self, strategy: adversarial_noise_strategy) -> None:
+    def __init__(self, strategy: AdversarialNoiseStrategy) -> None:
         """
         Usually, the Context accepts a strategy through the constructor, but
         also provides a setter to change it at runtime.
@@ -34,7 +34,7 @@ class AdversarialNoiseContext():
         self._strategy = strategy
 
     @property
-    def strategy(self) -> adversarial_noise_strategy:
+    def strategy(self) -> AdversarialNoiseStrategy:
         """
         The Context maintains a reference to one of the Strategy objects. The
         Context does not know the concrete class of a strategy. It should work
@@ -44,7 +44,7 @@ class AdversarialNoiseContext():
         return self._strategy
 
     @strategy.setter
-    def strategy(self, strategy: adversarial_noise_strategy) -> None:
+    def strategy(self, strategy: AdversarialNoiseStrategy) -> None:
         """
         Usually, the Context allows replacing a Strategy object at runtime.
         """
@@ -64,7 +64,7 @@ class AdversarialNoiseContext():
 
 ##### Different types of adversarial noise supported #####
 
-class uniform_noise(adversarial_noise_strategy):
+class UniformNoise(AdversarialNoiseStrategy):
     """
     The algorithm that adds noise to the data from a uniform distribution. 
     In a uniform distribution all values are equally likely. 
@@ -76,7 +76,7 @@ class uniform_noise(adversarial_noise_strategy):
     def get_name(self):
         return "Uniform noise"
 
-class gaussian_noise(adversarial_noise_strategy):
+class GaussianNoise(AdversarialNoiseStrategy):
     """
     The algorithm that adds noise to the data from a gaussian distribution.
     A Gaussian distribution is symmetric around the mean. It is also called the bell curve or normal distribution. 
@@ -88,7 +88,7 @@ class gaussian_noise(adversarial_noise_strategy):
     def get_name(self):
         return "Gaussian noise"
 
-class laplacian_noise(adversarial_noise_strategy):
+class LaplacianNoise(AdversarialNoiseStrategy):
     """
     The algorithm that adds noise to the data from a laplacian distribution.
     A Laplacian distribution is symmetric around the mean, however it is more concentrated near the mean than a gaussian distribution.
@@ -109,18 +109,18 @@ if __name__ == "__main__":
     original_data = np.array([1, 2, 3, 4, 5])
     print("Original data: ", original_data)
 
-    context = AdversarialNoiseContext(laplacian_noise())
+    context = AdversarialNoiseContext(LaplacianNoise())
     print("Client: Strategy is set to laplacian noise.")
     print("Data after noise: ", context.add_noise(original_data))
     print()
 
     original_data = np.array([[1, 2, 3, 4, 5], [3.2, 4.5, 6, 7.1, 2.4]])
-    context.strategy = gaussian_noise()
+    context.strategy = GaussianNoise()
     print("Client: Strategy is set to gaussian noise.")
     print("Data after noise: ", context.add_noise(original_data))
     print()
 
     original_data = np.array([[1], [2], [3], [4], [5]])
-    context.strategy = uniform_noise()
+    context.strategy = UniformNoise()
     print("Client: Strategy is set to uniform noise.")
     print("Data after noise: ", context.add_noise(original_data))
