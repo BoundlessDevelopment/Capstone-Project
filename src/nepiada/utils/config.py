a69c658b88ea6101f3c31c073f5e31f2a8f0f5f9
Thanoshan <36831115+Thanoshan@users.noreply.github.com>
2023-10-30 15:40:25 -0400

    document config

--- src/nepiada/utils/config.py
+++ src/nepiada/utils/config.py
@@ -4,4 +4,6 @@ class Config:
     num_adversarial_agents: int = 2
     obs_radius: int = 20
     iterations: int = 100
+    # Possible moves for each drone. Key is the action, value is the (dx, dy) tuple
+    # Default: 0 - stay, 1 - up, 2 - down, 3 - left, 4 - right
     possible_moves : {int : int} = {0 : (0, 0), 1 : (0, 1), 2 : (0, -1), 3 : (-1, 0), 4 : (1, 0)}
