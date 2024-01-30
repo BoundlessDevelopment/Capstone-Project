1a3d1b4933da2e7cbb4bb41dfec76cf6efe3dd86
Thanoshan <36831115+Thanoshan@users.noreply.github.com>
2024-01-29 23:30:59 -0500

    add MAPPO RL alg, NOT functional yet -- model yet to be specified. committing for cross-device access

---
+++ src/nepiada/rl_ma_ppo_experimental.py
@@ -0,0 +1,28 @@
+import ray
+import supersuit as ss
+from ray import tune
+from ray.rllib.algorithms.ppo import PPOConfig
+from ray.rllib.env.wrappers.pettingzoo_env import ParallelPettingZooEnv
+from ray.tune.registry import register_env
+
+import env.nepiada as nepiada
+from utils.config import Config
+
+def env_creator(args):
+    nepiada_config = Config()
+    env = nepiada.parallel_env(config=nepiada_config)
+   # env = ss.dtype_v0(env, "float32")
+    return env
+
+if __name__ == "__main__":
+    ray.init()
+
+    register_env("nepiada", lambda config: ParallelPettingZooEnv(env_creator(config)))
+
+    config = PPOConfig()
+    config = config.training(gamma=0.9, lr=0.01, kl_coeff=0.3, train_batch_size=128)
+    config = config.resources(num_gpus=0)
+    config = config.rollouts(num_rollout_workers=1)
+
+    algo = config.build(env="nepiada")
+    algo.train()
