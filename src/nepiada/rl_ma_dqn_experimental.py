dd7c0823944c3bc88a1f7e16ce701db44ceb09bc
Thanoshan <36831115+Thanoshan@users.noreply.github.com>
2024-02-02 10:25:43 -0500

    updates to algs

--- src/nepiada/rl_ma_dqn_experimental.py
+++ src/nepiada/rl_ma_dqn_experimental.py
@@ -14,36 +14,40 @@ def env_creator(args):
     nepiada_config = Config()
     env = nepiada.parallel_env(config=nepiada_config)
     # env = ss.dtype_v0(env, "float32")
-    return env
+    return ParallelPettingZooEnv(env)
 
 
 if __name__ == "__main__":
     ray.init()
 
-   # register_env("nepiada", lambda config: ParallelPettingZooEnv(env_creator(config)))
-    register_env("nepiada", lambda config: ParallelPettingZooEnv(env_creator(config)))
+    env = env_creator({})
+    register_env("nepiada", env_creator)
 
     config = DQNConfig()
 
-    # replay_config = {
-    #         "type": "MultiAgentPrioritizedReplayBuffer",
-    #         "capacity": 60000,
-    #         "prioritized_replay_alpha": 0.5,
-    #         "prioritized_replay_beta": 0.5,
-    #         "prioritized_replay_eps": 3e-6,
-    #     }
+    replay_config = {
+            "type": "MultiAgentPrioritizedReplayBuffer",
+            "capacity": 60000,
+            "prioritized_replay_alpha": 0.5,
+            "prioritized_replay_beta": 0.5,
+            "prioritized_replay_eps": 3e-6,
+        }
 
-    # config = config.training(replay_buffer_config=replay_config, num_atoms=tune.grid_search([1,]))
-    # config = config.resources(num_gpus=1)
-    # config = config.rollouts(num_rollout_workers=11)
-    # config = config.environment("nepiada")
-    # # algo = DQN(config=config)
-    # # algo.train()
-    # tune.Tuner(
-    #     "DQN",
-    #     run_config=air.RunConfig(stop={"training_iteration":175}, checkpoint_config=train.CheckpointConfig(checkpoint_frequency=30)),
-    #     param_space=config.to_dict()
-    # ).fit()
+    config = config.training(replay_buffer_config=replay_config, num_atoms=tune.grid_search([1,]))
+    config = config.resources(num_gpus=1)
+    config = config.rollouts(num_rollout_workers=11)
+    config = config.environment("nepiada")
+    config = config.multi_agent(policies=env.get_agent_ids(), policy_mapping_fn=(lambda agent_id, *args, **kwargs: agent_id))
+
+    # algo = DQN(config=config)
+    # algo.train()
+
+    stop = {"episodes_total": 60000}
+    tune.Tuner(
+        "DQN",
+        run_config=air.RunConfig(stop=stop, checkpoint_config=train.CheckpointConfig(checkpoint_frequency=50)),
+        param_space=config
+    ).fit()
 
     # DQN_agent = DQN.from_checkpoint("C:/Users/thanos/RL_experimental/DQN_2024-02-01_21-40-46/DQN_nepiada_77c1a_00000_0_num_atoms=1_2024-02-01_21-40-46/checkpoint_000005")
     # env = env_creator(None)
--- src/nepiada/rl_ma_ppo_experimental.py
+++ src/nepiada/rl_ma_ppo_experimental.py
@@ -13,31 +13,38 @@ def env_creator(args):
     nepiada_config = Config()
     env = nepiada.parallel_env(config=nepiada_config)
     # env = ss.dtype_v0(env, "float32")
-    return env
+    return ParallelPettingZooEnv(env)
 
 
 if __name__ == "__main__":
     ray.init()
-
-    register_env("nepiada", lambda config: ParallelPettingZooEnv(env_creator(config)))
+    
+    env = env_creator({})
+    register_env("nepiada", env_creator)
 
     config = PPOConfig()
     config = config.training(gamma=0.9, lr=0.01, kl_coeff=0.3, train_batch_size=128)
     config = config.resources(num_gpus=1)
-    config = config.rollouts(num_rollout_workers=1)
+    config = config.rollouts(num_rollout_workers=10)
+    config = config.multi_agent(policies=env.get_agent_ids(), policy_mapping_fn=(lambda agent_id, *args, **kwargs: agent_id))
+
 
     # algo = config.build(env="nepiada")
     # algo.train()
+
+    # stop = {"episodes_total": 60000}
 
     # config = config.environment(env="nepiada")
 
     # tune.Tuner(
     #     "PPO",
     #     run_config=air.RunConfig(
-    #         stop={"training_iteration": 10},
-    #         checkpoint_config=train.CheckpointConfig(checkpoint_frequency=10),
+    #         stop=stop,
+    #         checkpoint_config=air.CheckpointConfig(
+    #             checkpoint_frequency=30,
+    #         ),
     #     ),
-    #     param_space=config.to_dict(),
+    #     param_space=config,
     # ).fit()
 
     # tune.run(
