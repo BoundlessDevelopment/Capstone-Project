735193f6b9c8cef8da940f3af7322aa37c1539b5
Thanoshan <36831115+Thanoshan@users.noreply.github.com>
2024-02-07 12:25:05 -0500

    restore original DQN and make rainbow seperate

--- src/nepiada/rl_ma_dqn_rainbow_experimental.py
+++ src/nepiada/rl_ma_dqn_rainbow_experimental.py
@@ -28,18 +28,17 @@ if __name__ == "__main__":
 
     replay_config = {
             "type": "MultiAgentPrioritizedReplayBuffer",
-            "capacity": 60000,
+            "capacity": 50000,
             "prioritized_replay_alpha": 0.5,
             "prioritized_replay_beta": 0.5,
             "prioritized_replay_eps": 3e-6,
         }
 
-    config = config.training(replay_buffer_config=replay_config, num_atoms=tune.grid_search([1,]))
+    config = config.training(replay_buffer_config=replay_config, num_atoms=51, n_step=3, noisy=True, v_min=-10.0, v_max=10.0, gamma=0.99, lr=0.0001, train_batch_size=32)
     config = config.resources(num_gpus=1)
-    config = config.rollouts(num_rollout_workers=11)
+    config = config.rollouts(num_rollout_workers=11, rollout_fragment_length=4, compress_observations=True)
     config = config.environment("nepiada")
-    config = config.multi_agent(policies=env.get_agent_ids(), policy_mapping_fn=(lambda agent_id, *args, **kwargs: agent_id))
-   # config = config.exploration(explore=True, exploration_config={"type": "EpsilonGreedy", "initial_epsilon": 1.0, "final_epsilon": 0.01, "epsilon_timesteps": 10000})
+    config = config.exploration(explore=True, exploration_config={"type": "EpsilonGreedy", "initial_epsilon": 1.0, "final_epsilon": 0.01, "epsilon_timesteps": 10000})
 
     # algo = DQN(config=config)
     # algo.train()
