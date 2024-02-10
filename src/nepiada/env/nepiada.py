cf2a9c51abd1212021ba271ccb4a522c55882cc1
Thanoshan <36831115+Thanoshan@users.noreply.github.com>
2024-02-09 21:56:59 -0500

    add rewards decay linearly, can be tweaked or removed

--- src/nepiada/env/nepiada.py
+++ src/nepiada/env/nepiada.py
@@ -461,6 +461,11 @@ class nepiada(ParallelEnv):
             else:
                 # -10 reward for agents that collided with boundary
                 rewards[agent_name] = -10
+
+        # THANOS EXPERIMENTAL - Linear decay with num moves, to a minimum of -2 (at iteration 50, a reward of 10 will actually be a reward -2)
+        for agent_name in self.agents:
+            linear_decay_rate = (rewards[agent_name] - -2) / self.config.iterations
+            rewards[agent_name] = min(rewards[agent_name], rewards[agent_name] - (linear_decay_rate * self.num_moves))
         
         self._store_scores_in_agent(curr_scores)
         return rewards
@@ -518,62 +523,62 @@ class nepiada(ParallelEnv):
             self.world.agents[agent_name].prev_score = scores[agent_name]
 
     # THANOS EXPERIMENTAL
-    def get_rewards_old(self):
-        """
-        This function assigns reward to all agents based on the following two criterias:
+    # def get_rewards_old(self):
+    #     """
+    #     This function assigns reward to all agents based on the following two criterias:
 
-        - global_arrangement_reward : The average distance of all agents from the target
-        - local_arrangement_reward : The deviation of the agent from the ideal arrangement with it's target neighbours
+    #     - global_arrangement_reward : The average distance of all agents from the target
+    #     - local_arrangement_reward : The deviation of the agent from the ideal arrangement with it's target neighbours
 
-        Refer to D. Gadjov and Pavel's paper for more details about it.
+    #     Refer to D. Gadjov and Pavel's paper for more details about it.
 
-        Returns: A dictionary with agent_name as key and reward as a value
-        """
-        rewards = {}
+    #     Returns: A dictionary with agent_name as key and reward as a value
+    #     """
+    #     rewards = {}
 
-        # Get the average vector distance of the agents from target
-        target_x = self.config.size / 2
-        target_y = self.config.size / 2
-        global_arrangement_vector = np.array([0.0, 0.0])
+    #     # Get the average vector distance of the agents from target
+    #     target_x = self.config.size / 2
+    #     target_y = self.config.size / 2
+    #     global_arrangement_vector = np.array([0.0, 0.0])
 
-        for agent_name in self.agents:
-            agent = self.world.agents[agent_name]
-            global_arrangement_vector += np.array(
-                [(agent.p_pos[0] - target_x), (target_y - agent.p_pos[1])]
-            )
+    #     for agent_name in self.agents:
+    #         agent = self.world.agents[agent_name]
+    #         global_arrangement_vector += np.array(
+    #             [(agent.p_pos[0] - target_x), (target_y - agent.p_pos[1])]
+    #         )
 
-        global_arrangement_vector = np.divide(
-            global_arrangement_vector, len(self.agents)
-        )
-        global_arrangement_reward = np.sqrt(
-            global_arrangement_vector[0] ** 2 + global_arrangement_vector[1] ** 2
-        )
+    #     global_arrangement_vector = np.divide(
+    #         global_arrangement_vector, len(self.agents)
+    #     )
+    #     global_arrangement_reward = np.sqrt(
+    #         global_arrangement_vector[0] ** 2 + global_arrangement_vector[1] ** 2
+    #     )
 
-        # We update the global arrangement vector in the graph to visually inspect the global centroid of the agents
-        self.world.graph.global_arrangement_vector = global_arrangement_vector
+    #     # We update the global arrangement vector in the graph to visually inspect the global centroid of the agents
+    #     self.world.graph.global_arrangement_vector = global_arrangement_vector
 
-        # Add each agents reward based on their target neighbours
-        for agent_name in self.agents:
-            agent = self.world.agents[agent_name]
-            agent_x = agent.p_pos[0]
-            agent_y = agent.p_pos[1]
-            deviation_from_arrangement = 0
-            for neighbour_name, ideal_distance in agent.target_neighbour.items():
-                neighbour = self.world.agents[neighbour_name]
-                neighbour_x = neighbour.p_pos[0]
-                neighbour_y = neighbour.p_pos[1]
-                ideal_x = ideal_distance[0]
-                ideal_y = ideal_distance[1]
+    #     # Add each agents reward based on their target neighbours
+    #     for agent_name in self.agents:
+    #         agent = self.world.agents[agent_name]
+    #         agent_x = agent.p_pos[0]
+    #         agent_y = agent.p_pos[1]
+    #         deviation_from_arrangement = 0
+    #         for neighbour_name, ideal_distance in agent.target_neighbour.items():
+    #             neighbour = self.world.agents[neighbour_name]
+    #             neighbour_x = neighbour.p_pos[0]
+    #             neighbour_y = neighbour.p_pos[1]
+    #             ideal_x = ideal_distance[0]
+    #             ideal_y = ideal_distance[1]
 
-                deviation_from_arrangement += np.sqrt(
-                    (neighbour_x - agent_x - ideal_x) ** 2
-                    + (neighbour_y - agent_y - ideal_y) ** 2
-                )
+    #             deviation_from_arrangement += np.sqrt(
+    #                 (neighbour_x - agent_x - ideal_x) ** 2
+    #                 + (neighbour_y - agent_y - ideal_y) ** 2
+    #             )
 
-            # Compute the agent's net reward, note the negative sign
-            rewards[agent_name] = -(
-                (self.config.global_reward_weight * global_arrangement_reward)
-                + (self.config.local_reward_weight * deviation_from_arrangement)
-            )
+    #         # Compute the agent's net reward, note the negative sign
+    #         rewards[agent_name] = -(
+    #             (self.config.global_reward_weight * global_arrangement_reward)
+    #             + (self.config.local_reward_weight * deviation_from_arrangement)
+    #         )
 
-        return rewards
+    #     return rewards
